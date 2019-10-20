4566f87547a5423b1cd8e36283e9b837e99f7b4c
Deepanshu Sharma <15ucs167@lnmiit.ac.in>
2019-10-20 15:03:28 +0530

    Import archive in a Pandas DataFrame

---
+++ wrangle_twitter_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Create a function for gathering data
+
+def gather_data():
+
+    # Put archive in a Pandas DataFrame
+    archive = pd.read_csv('twitter-archive-enhanced.csv')
+
+    
+
+def main():
+    gather_data()
+
+if __name__ == '__main__':
+    main()
